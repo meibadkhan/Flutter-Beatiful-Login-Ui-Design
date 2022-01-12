c7b95982eb3dd556d97916457e89baafacf60859
Ibaadkhan <meibadkhan@gmail.com>
2022-01-12 12:57:16 +0500

    flutter login App

---
+++ lib/main.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+import 'RegisterScreen.dart';
+
+main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Login(),
+  ));
+}
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Stack(
+        children: [
+          Opacity(opacity: 0.56, child: Image.asset("images/man2.jpg")),
+          Positioned(
+            top: 230,
+            left: 40,
+            child: Text(
+              "Sign In",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 500,
+            padding: EdgeInsets.only(top: 15, left: 15, right: 15),
+            margin: EdgeInsets.only(top: 300, left: 20, right: 20),
+            decoration: BoxDecoration(
+                color: Colors.grey.shade400.withOpacity(0.5),
+                borderRadius: BorderRadius.circular(10)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "E-mail",
+                      fillColor: Colors.white70,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      filled: true),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Password",
+                      fillColor: Colors.white70,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      filled: true),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                ElevatedButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.indigoAccent,
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 143, vertical: 15)),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Home()));
+                    },
+                    child: Text(
+                      "Continue",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    )),
+                Center(
+                  child: CircleAvatar(
+                    backgroundColor: Colors.black12,
+                    child: Text(
+                      "Or",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.only(left: 12),
+                    height: 55,
+                    decoration: BoxDecoration(
+                        color: Colors.white70,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          "images/googleicon.png",
+                          width: 30,
+                          height: 30,
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          "Continue With Google ",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: EdgeInsets.only(left: 12),
+                    width: double.infinity,
+                    height: 55,
+                    decoration: BoxDecoration(
+                        color: Colors.white70,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          "images/fb.png",
+                          width: 35,
+                          height: 35,
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          "Continue With Facebook ",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Don't have an account",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Register()));
+                        },
+                        child: Text(
+                          "Sign Up?",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ))
+                  ],
+                ),
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Forget Your Password?",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ))
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigoAccent,
+        title: Text("Home Screen"),
+      ),
+    );
+  }
+}
